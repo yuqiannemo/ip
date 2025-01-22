289332c7bcd7e08e821d2c8002ed654a240ebce7
yuqiannemo <e1300534@u.nus.edu>
2025-01-22 17:14:50 +0800

    rename, greet and exit

---
+++ src/main/java/Nemo.java
@@ -0,0 +1,13 @@
+public class Nemo {
+    public static void main(String[] args) {
+        String divider = "_".repeat(60);
+        String greeting = "Hello I am Nemo, a very friendly and smart fish yay :) ";
+        String question = "What can I do for you?";
+        String farewell = "Bye Bye, see you soon!";
+        System.out.println(divider);
+        System.out.println(greeting);
+        System.out.println(question);
+        System.out.println(divider);
+        System.out.println(farewell);
+    }
+}
