49722cd4386b330204fa915e525f84e86117471b
yuqiannemo <e1300534@u.nus.edu>
2025-01-22 19:26:33 +0800

    add echo

--- src/main/java/Nemo.java
+++ src/main/java/Nemo.java
@@ -1,13 +1,34 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Nemo {
+    String divider = "_".repeat(60);
+    String greeting = "Hello I am Nemo, a very friendly and smart fish yay :) ";
+    String question = "What can I do for you?";
+    String farewell = "Bye Bye, see you soon!";
+    public void Echo(String message) {
+        if (Objects.equals(message, "bye")) {
+            message = farewell;
+        }
+        System.out.println("   " + divider);
+        System.out.println("   " + message);
+        System.out.println("   " + divider);
+
+        if (Objects.equals(message, farewell)) {
+            System.exit(0);
+        }
+    }
     public static void main(String[] args) {
-        String divider = "_".repeat(60);
-        String greeting = "Hello I am Nemo, a very friendly and smart fish yay :) ";
-        String question = "What can I do for you?";
-        String farewell = "Bye Bye, see you soon!";
-        System.out.println(divider);
-        System.out.println(greeting);
-        System.out.println(question);
-        System.out.println(divider);
-        System.out.println(farewell);
+        Nemo nemo = new Nemo();
+        System.out.println("   " + nemo.divider);
+        System.out.println("   " + nemo.greeting);
+        System.out.println("   " + nemo.question);
+        System.out.println("   " + nemo.divider);
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String message = scanner.nextLine();
+            nemo.Echo(message);
+        }
     }
 }
