732a6fa833c6d8ea65526070ecc77b4adaf48d5f
yuqiannemo <e1300534@u.nus.edu>
2025-01-22 19:53:41 +0800

    add store and list feature

--- src/main/java/Nemo.java
+++ src/main/java/Nemo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -6,18 +7,39 @@ public class Nemo {
     String greeting = "Hello I am Nemo, a very friendly and smart fish yay :) ";
     String question = "What can I do for you?";
     String farewell = "Bye Bye, see you soon!";
+    ArrayList<String> events = new ArrayList<String>();
+
     public void Echo(String message) {
-        if (Objects.equals(message, "bye")) {
-            message = farewell;
-        }
         System.out.println("   " + divider);
         System.out.println("   " + message);
         System.out.println("   " + divider);
+    }
 
-        if (Objects.equals(message, farewell)) {
+    public void List() {
+        System.out.println("   " + divider);
+        int count = 1;
+        for (String event : events) {
+            System.out.println("   " + count + ". " + event);
+            count++;
+        }
+        System.out.println("   " + divider);
+    }
+
+    public void Add(String event) {
+        if (Objects.equals(event, "bye")) {
+            event = farewell;
+        }
+
+        events.add(event);
+        System.out.println("   " + divider);
+        System.out.println("   added: " + event);
+        System.out.println("   " + divider);
+
+        if (Objects.equals(event, farewell)) {
             System.exit(0);
         }
     }
+
     public static void main(String[] args) {
         Nemo nemo = new Nemo();
         System.out.println("   " + nemo.divider);
@@ -28,7 +50,18 @@ public class Nemo {
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String message = scanner.nextLine();
-            nemo.Echo(message);
+            if (Objects.equals(message, "list")) {
+                nemo.List();
+                continue;
+            }
+            if (Objects.equals(message, "bye")) {
+                message = nemo.farewell;
+                System.out.println("   " + nemo.divider);
+                System.out.println("   " + message);
+                System.out.println("   " + nemo.divider);
+                System.exit(0);
+            }
+            nemo.Add(message);
         }
     }
 }
