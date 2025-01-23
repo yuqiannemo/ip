e6012e884a4668d9db81b283d3b61ba2434da3e4
yuqiannemo <e1300534@u.nus.edu>
2025-01-23 16:33:07 +0800

    use switch syntax for 3 tasks and handle exceptions

--- src/main/java/Nemo.java
+++ src/main/java/Nemo.java
@@ -23,8 +23,7 @@ public class Nemo {
         if (index < 1 || index > tasks.size()) {
             System.out.println("   " + divider);
             System.out.println("   invalid index");
-        }
-        else {
+        } else {
             tasks.get(index - 1).markAsDone();
             System.out.println("   " + divider);
             System.out.println("   Nice! I've marked this task as done:");
@@ -37,8 +36,7 @@ public class Nemo {
         if (index < 1 || index > tasks.size()) {
             System.out.println("   " + divider);
             System.out.println("   invalid index");
-        }
-        else {
+        } else {
             tasks.get(index - 1).markAsUndone();
             System.out.println("   " + divider);
             System.out.println("   Okay! I've marked this task as not done yet:");
@@ -56,6 +54,54 @@ public class Nemo {
         System.out.println("   " + divider);
     }
 
+    public void handleTask(String message, String command) {
+        try {
+            switch (command) {
+                case "todo" -> {
+                    String description = message.substring(4).trim();
+                    if (description.isEmpty()) {
+                        throw new NemoException("Opps :( Todo cannot be empty!");
+                    }
+                    ToDo todo = new ToDo(description);
+                    addTask(todo);
+                }
+                case "deadline" -> {
+                    if (!message.contains("/by")) {
+                        throw new NemoException("Opps :( Deadline must include '/by'!");
+                    }
+                    String by = message.substring(message.indexOf("/by") + 4).trim();
+                    String description = message.substring(9, message.indexOf(" /by")).trim();
+                    if (description.isEmpty() || by.isEmpty()) {
+                        throw new NemoException("Deadline description or date cannot be empty!");
+                    }
+                    Deadline deadline = new Deadline(description, by);
+                    addTask(deadline);
+                }
+                case "event" -> {
+                    if (!message.contains("/from") || !message.contains("/to")) {
+                        throw new NemoException("Opps :( Event must include '/from' and '/to'!");
+                    }
+                    String from = message.substring(message.indexOf("/from") + 6, message.indexOf(" /to")).trim();
+                    String to = message.substring(message.indexOf("/to") + 4).trim();
+                    String description = message.substring(6, message.indexOf(" /from")).trim();
+                    if (description.isEmpty() || from.isEmpty() || to.isEmpty()) {
+                        throw new NemoException("Opps :( Event description, from, or to fields cannot be empty!");
+                    }
+                    Event event = new Event(description, from, to);
+                    addTask(event);
+                }
+                default -> {
+                    throw new NemoException("I don't know what does that mean... Try typing todo/event/deadline " +
+                            "for task management");
+                }
+            }
+        } catch (NemoException e) {
+            System.out.println("   " + divider);
+            System.out.println("   " + e.toString());
+            System.out.println("   " + divider);
+        }
+    }
+
     public static void main(String[] args) {
         Nemo nemo = new Nemo();
         System.out.println("   " + nemo.divider);
@@ -70,44 +116,18 @@ public class Nemo {
             String command = messageArray[0];
             if (Objects.equals(message, "list")) {
                 nemo.List();
-            }
-            else if (Objects.equals(message, "bye")) {
-                message = nemo.farewell;
+            } else if (Objects.equals(message, "bye")) {
                 System.out.println("   " + nemo.divider);
-                System.out.println("   " + message);
+                System.out.println("   " + nemo.farewell);
                 System.out.println("   " + nemo.divider);
                 System.exit(0);
-            }
-            else if (Objects.equals(command, "mark")) {
+            } else if (Objects.equals(command, "mark")) {
                 nemo.MarkAsDone(Integer.parseInt(messageArray[1]));
             } else if (Objects.equals(command, "unmark")) {
                 nemo.MarkAsUndone(Integer.parseInt(messageArray[1]));
             } else {
-                if (Objects.equals(command, "todo")) {
-//                retrieve the description
-                    String description = message.substring(5);
-                    ToDo todo = new ToDo(description);
-                    nemo.addTask(todo);
-                } else if (Objects.equals(command, "deadline")) {
-//                retrieve the string after "/by"
-                    String by = message.substring(message.indexOf("/by") + 4);
-//                retrieve the description
-                    String description = message.substring(9, message.indexOf(" /by"));
-                    Deadline deadline = new Deadline(description, by);
-                    nemo.addTask(deadline);
-                } else if (Objects.equals(command, "event")) {
-//                retrieve the "from"
-                    String from = message.substring(message.indexOf("/from") + 6, message.indexOf(" /to"));
-//                retrieve the "to"
-                    String to = message.substring(message.indexOf("/to") + 4);
-//                retrieve the description
-                    String description = message.substring(6, message.indexOf(" /from"));
-                    Event event = new Event(description, from, to);
-                    nemo.addTask(event);
-                } else {
-                    nemo.addTask(new Task(message));
-                }
-            }
+                nemo.handleTask(message, command);
             }
         }
     }
+}
