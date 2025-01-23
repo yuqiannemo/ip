e3fe2c41382a242fa7ef483641a9035b1ed081ae
yuqiannemo <e1300534@u.nus.edu>
2025-01-23 16:48:17 +0800

    handle exceptions for mark and unmark

--- src/main/java/Nemo.java
+++ src/main/java/Nemo.java
@@ -11,6 +11,9 @@ public class Nemo {
 
     public void List() {
         System.out.println("   " + divider);
+        if (tasks.isEmpty()) {
+            System.out.println("   No tasks now, take a rest and come back later :)");
+        }
         int count = 1;
         for (Task task : tasks) {
             System.out.println("   " + count + ". " + task.toString());
@@ -19,28 +22,34 @@ public class Nemo {
         System.out.println("   " + divider);
     }
 
-    public void MarkAsDone(int index) {
-        if (index < 1 || index > tasks.size()) {
-            System.out.println("   " + divider);
-            System.out.println("   invalid index");
-        } else {
-            tasks.get(index - 1).markAsDone();
-            System.out.println("   " + divider);
-            System.out.println("   Nice! I've marked this task as done:");
-            System.out.println("      " + tasks.get(index - 1).toString());
-            System.out.println("   " + divider);
-        }
-    }
+    public void updateTaskStatus(String command, String indexStr) {
+        try {
+            int index;
+            try {
+                index = Integer.parseInt(indexStr);
+            } catch (NumberFormatException e) {
+                throw new NemoException("Opps :( Invalid index! Maybe try to provide a valid number after '" + command + "'?");
+            }
 
-    public void MarkAsUndone(int index) {
-        if (index < 1 || index > tasks.size()) {
+            if (index < 1 || index > tasks.size()) {
+                throw new NemoException("Opps :( Task number " + index + " does not exist in the list...");
+            }
+
+            Task task = tasks.get(index - 1);
+            if (command.equals("mark")) {
+                task.markAsDone();
+                System.out.println("   " + divider);
+                System.out.println("   Nice! I've marked this task as done:");
+            } else {
+                task.markAsUndone();
+                System.out.println("   " + divider);
+                System.out.println("   Okay! I've marked this task as not done yet:");
+            }
+            System.out.println("      " + task.toString());
             System.out.println("   " + divider);
-            System.out.println("   invalid index");
-        } else {
-            tasks.get(index - 1).markAsUndone();
+        } catch (NemoException e) {
             System.out.println("   " + divider);
-            System.out.println("   Okay! I've marked this task as not done yet:");
-            System.out.println("      " + tasks.get(index - 1).toString());
+            System.out.println("   " + e.toString());
             System.out.println("   " + divider);
         }
     }
@@ -67,7 +76,7 @@ public class Nemo {
                 }
                 case "deadline" -> {
                     if (!message.contains("/by")) {
-                        throw new NemoException("Opps :( Deadline must include '/by'!");
+                        throw new NemoException("Opps :( Deadline needs to include '/by'!");
                     }
                     String by = message.substring(message.indexOf("/by") + 4).trim();
                     String description = message.substring(9, message.indexOf(" /by")).trim();
@@ -79,7 +88,7 @@ public class Nemo {
                 }
                 case "event" -> {
                     if (!message.contains("/from") || !message.contains("/to")) {
-                        throw new NemoException("Opps :( Event must include '/from' and '/to'!");
+                        throw new NemoException("Opps :( Event needs to include '/from' and '/to'!");
                     }
                     String from = message.substring(message.indexOf("/from") + 6, message.indexOf(" /to")).trim();
                     String to = message.substring(message.indexOf("/to") + 4).trim();
@@ -91,8 +100,7 @@ public class Nemo {
                     addTask(event);
                 }
                 default -> {
-                    throw new NemoException("I don't know what does that mean... Try typing todo/event/deadline " +
-                            "for task management");
+                    throw new NemoException("Sorry, Nemo does not know...");
                 }
             }
         } catch (NemoException e) {
@@ -121,10 +129,14 @@ public class Nemo {
                 System.out.println("   " + nemo.farewell);
                 System.out.println("   " + nemo.divider);
                 System.exit(0);
-            } else if (Objects.equals(command, "mark")) {
-                nemo.MarkAsDone(Integer.parseInt(messageArray[1]));
-            } else if (Objects.equals(command, "unmark")) {
-                nemo.MarkAsUndone(Integer.parseInt(messageArray[1]));
+            } else if (Objects.equals(command, "mark") || Objects.equals(command, "unmark")) {
+                if (messageArray.length < 2) {
+                    System.out.println("   " + nemo.divider);
+                    System.out.println("   Opps, please specify a task number after '" + command + "'.");
+                    System.out.println("   " + nemo.divider);
+                } else {
+                    nemo.updateTaskStatus(command, messageArray[1]);
+                }
             } else {
                 nemo.handleTask(message, command);
             }
