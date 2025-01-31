50d2fc5a506ee397a5a5baa5678991059a8dae6b
yuqiannemo <e1300534@u.nus.edu>
2025-01-31 14:32:27 +0800

    Add TaskList class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class TaskList {
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public void add(Task task) {
+        tasks.add(task);
+    }
+
+    public void delete(int index) throws NemoException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new NemoException("Task number " + (index + 1) + " does not exist in the list...");
+        }
+        tasks.remove(index);
+    }
+
+    public Task get(int index) throws NemoException {
+        if (index < 0 || index >= tasks.size()) {
+            throw new NemoException("Task number " + (index + 1) + " does not exist in the list...");
+        }
+        return tasks.get(index);
+    }
+
+    public int size() {
+        return tasks.size();
+    }
 }
