ab5dc3fc7d0363768f8b2eadcaa41a2ab5487d31
yuqiannemo <e1300534@u.nus.edu>
2025-01-31 19:23:33 +0800

    Add JavaDoc for task classes

--- src/main/java/nemo/task/TaskList.java
+++ src/main/java/nemo/task/TaskList.java
@@ -4,13 +4,23 @@ import nemo.NemoException;
 
 import java.util.ArrayList;
 
+/**
+ * Manages a list of tasks.
+ */
 public class TaskList {
     private ArrayList<Task> tasks;
 
+    /**
+     * Constructs an empty TaskList.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Constructs a TaskList with existing tasks.
+     * @param tasks The list of tasks.
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
@@ -19,14 +29,27 @@ public class TaskList {
         return tasks;
     }
 
+    /**
+     * Checks if the task list is empty.
+     * @return True if the task list is empty, false otherwise.
+     */
     public boolean isEmpty() {
         return tasks.isEmpty();
     }
 
+    /**
+     * Adds a task to the list.
+     * @param task The task to add.
+     */
     public void add(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Deletes a task from the list by index.
+     * @param index The index of the task to delete.
+     * @throws NemoException If the index is out of bounds.
+     */
     public void delete(int index) throws NemoException {
         if (index < 0 || index >= tasks.size()) {
             throw new NemoException("nemo.task.Task number " + (index + 1) + " does not exist in the list...");
@@ -34,6 +57,12 @@ public class TaskList {
         tasks.remove(index);
     }
 
+    /**
+     * Retrieves a task from the list by index.
+     * @param index The index of the task.
+     * @return The task at the given index.
+     * @throws NemoException If the index is out of bounds.
+     */
     public Task get(int index) throws NemoException {
         if (index < 0 || index >= tasks.size()) {
             throw new NemoException("nemo.task.Task number " + (index + 1) + " does not exist in the list...");
@@ -41,6 +70,10 @@ public class TaskList {
         return tasks.get(index);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     * @return The size of the task list.
+     */
     public int size() {
         return tasks.size();
     }
